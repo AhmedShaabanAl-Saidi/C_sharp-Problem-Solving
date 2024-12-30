551c566e6f0a48b8be81caa93f755e62cca91508
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2024-12-30 13:29:52 +0200

    Printnamefromuser

--- Program.cs
+++ Program.cs
@@ -2,9 +2,37 @@
 {
     internal class Program
     {
+        //// 1- Writeyourname
+        //public static void PrintName()
+        //{
+        //    Console.WriteLine("Welcome in Print Name App :");
+        //    PrintMassage("Ahmed Shaaban");
+        //}
+
+        //// 2- Printnamefromuser
+        //public static void PrintNameFromUser()
+        //{
+        //    Console.WriteLine("Welcome in Print User Name App :");
+        //    Console.WriteLine("Please Enter Your Name :");
+        //    string userName = Console.ReadLine();
+        //    PrintMassage(userName);
+        //}
+
+        //// Print Massage 
+        //public static void PrintMassage(string massage)
+        //{
+        //    Console.WriteLine("--------------------------------");
+        //    Console.WriteLine($"Your Name is : {massage}");
+        //    Console.WriteLine("--------------------------------");
+        //}
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // 1- Writeyourname
+            //PrintName();
+
+            // 2- Printnamefromuser
+            //PrintNameFromUser();
         }
     }
 }
