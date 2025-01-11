f19e4b353cd2a45077b9d47f10a60e23b4886106
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-01-11 18:42:28 +0200

    Avarage3numbers

--- Program.cs
+++ Program.cs
@@ -2,13 +2,16 @@
 {
     internal class Program
     {
+        #region Writeyourname Function
         //// 1- Writeyourname
         //public static void PrintName()
         //{
         //    Console.WriteLine("Welcome in Print Name App :");
         //    PrintMassage("Ahmed Shaaban");
-        //}
+        //} 
+        #endregion
 
+        #region Printnamefromuser Function
         //// 2- Printnamefromuser
         //public static void PrintNameFromUser()
         //{
@@ -16,33 +19,39 @@
         //    Console.WriteLine("Please Enter Your Name :");
         //    string userName = Console.ReadLine();
         //    PrintMassage(userName);
-        //}
+        //} 
+        #endregion
 
+        #region Print Massage Function
         // Print Massage 
-        //public static void PrintMassage(string massage)
-        //{
-        //    Console.WriteLine("--------------------------------");
-        //    Console.WriteLine(massage);
-        //    Console.WriteLine("--------------------------------");
-        //}
+        public static void PrintMassage(string massage)
+        {
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine(massage);
+            Console.WriteLine("--------------------------------");
+        }
+        #endregion
 
         ////////////////////////////////////////////////////////////
-        
-        //public static bool ReadNumder(string fieldNumber , out int number)
-        //{
-        //    bool Flag;
 
-        //    Console.Write($"Please Enter {fieldNumber} :");
-        //    Flag = int.TryParse(Console.ReadLine(), out number);
-        //    if (!Flag)
-        //    {
-        //         Console.WriteLine("Invalid Number Try Again");
-        //         return false;
-        //    }
+        #region ReadNumder Function
+        public static bool ReadNumder(string fieldNumber, out int number)
+        {
+            bool Flag;
 
-        //    return true;
-        //}
+            Console.Write($"Please Enter {fieldNumber} :");
+            Flag = int.TryParse(Console.ReadLine(), out number);
+            if (!Flag)
+            {
+                Console.WriteLine("Invalid Number Try Again");
+                return false;
+            }
 
+            return true;
+        }
+        #endregion
+
+        #region Sum2numbers Function
         // 3- Sum2numbers
         //public static void Sum2numbers()
         //{
@@ -54,9 +63,30 @@
 
         //    if (!ReadNumder("Number 2", out number2))
         //        return;
-            
+
         //    PrintMassage($"{number1} + {number2} = {number1 + number2}");
-        //}
+        //} 
+        #endregion
+
+        #region Avarage3numbers Function
+        // 4- Avarage3numbers
+        //public static void Avarage3numbers()
+        //{
+        //    Console.WriteLine("Welcome in Avarage Three Numbers App :");
+        //    int number1, number2, number3;
+
+        //    if (!ReadNumder("Number 1", out number1))
+        //        return;
+
+        //    if (!ReadNumder("Number 2", out number2))
+        //        return;
+
+        //    if (!ReadNumder("Number 3", out number3))
+        //        return;
+
+        //    PrintMassage($"{number1} + {number2} + {number3} / 3  = {(double)((number1 + number2 + number3) / 3)}");
+        //} 
+        #endregion
 
         static void Main(string[] args)
         {
@@ -70,6 +100,9 @@
 
             // 3 - Sum2numbers
             //Sum2numbers();
+
+            // 4- Avarage3numbers
+            //Avarage3numbers();
         }
     }
 }
